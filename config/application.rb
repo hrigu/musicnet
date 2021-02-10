dd93722182815acb89a4927101edb3e8912e0e9f
chrigu <christian.muehlethaler@optor.ch>
2021-02-10 11:45:56 +0100

    Zeiten in lokaler Zeitzone anzeigen

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,8 @@ module Musicnet
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Bern"
+
     # config.eager_load_paths << Rails.root.join("extras")
     #
     client_id = Rails.application.credentials.dig(:spotify, :client_id)
