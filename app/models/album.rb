f2cd77fcda7eba7e0186fbaf2c81ed3b0e883b21
chrigu <christian.muehlethaler@optor.ch>
2023-08-27 15:19:41 +0200

    Album: die Artists der Tracks. Nur einmal

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,6 +2,6 @@ class Album < ApplicationRecord
   # Die Tracks des Albums, die in mindestens einer Playlist vorhanden sind
   has_many :tracks
   # Die Künstler welche auf den Tracks
-  has_many :artists, through: :tracks
+  has_many :artists, -> { distinct }, through: :tracks
 
 end
