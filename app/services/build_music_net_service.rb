652c3953e17a4b5a7e335f634d9c37f95b7e53a3
chrigu <christian.muehlethaler@optor.ch>
2023-08-27 14:02:44 +0200

    build_music_net_service.rb umgebaut

--- app/services/build_music_net_service.rb
+++ app/services/build_music_net_service.rb
@@ -3,7 +3,7 @@ class BuildMusicNetService
     @current_user = current_user
   end
 
-  # Erstellt die ganze DB aus allen Playlists, die der Owner erstellt hat.
+  # Erstellt die ganze Modellstruktur aus allen Playlists, die der Owner erstellt hat.
   def build
     spotify_playlists = fetch_all_playlists_from_spotify
     spotify_playlists.each do |spot_playlist|
@@ -12,17 +12,16 @@ class BuildMusicNetService
     end
   end
 
-
-
   private
 
-  # Erstellt aus der spot_playlist, eine entsprechendes Playlist und speichert es in der DB
+  # Erstellt aus der spot_playlist eine entsprechendes Playlist und speichert es in der DB
   # Für jeden spot_track wird dann ein Track erstellt
   def build_playlist(spot_playlist)
     Rails.logger.info "build_playlist: #{spot_playlist.name}"
-    playlist = Playlist.find_by(spotify_id: spot_playlist.id)
-    unless playlist.present?
-      playlist = Playlist.create!(spotify_id: spot_playlist.id, snapshot_id: spot_playlist.snapshot_id, name: spot_playlist.name, public: spot_playlist.public)
+    playlist = Playlist.find_or_create_by!(spotify_id: spot_playlist.id) do |p|
+      p.snapshot_id = spot_playlist.snapshot_id
+      p.name = spot_playlist.name
+      p.public = spot_playlist.public
     end
 
     spot_playlist.tracks.each do |spot_track|
@@ -30,46 +29,44 @@ class BuildMusicNetService
     end
   end
 
-  # Für jedes
+  # Für jeden Spot_track in der spot_playlist wird, falls noch nicht vorhanden:
+  # * ein Track und ein PlaylistTrack
+  # * Ein Album
+  # * Die Artisten
+  # erstellt
   def build_track(playlist, spot_playlist, spot_track)
     Rails.logger.info " build_track: #{spot_track.name}"
-    track = Track.find_by(spotify_id: spot_track.id)
-    unless track.present?
+
+    track = Track.find_or_create_by!(spotify_id: spot_track.id) do |t|
       album = build_album(spot_track.album)
       artists = build_artists spot_track.artists
-
       popularity = try_fetch(spot_track, :popularity)
       audio_features = try_fetch(spot_track, :audio_features)
-      #album
-      track = Track.create!(
-        spotify_id: spot_track.id,
-        name: spot_track.name,
-        url: spot_track.external_urls["spotify"],
-        duration_ms: spot_track.duration_ms,
-        popularity: popularity,
-        audio_features: audio_features.to_json,
-        album: album,
-        artists: artists
-      )
+      t.name = spot_track.name
+      t.url = spot_track.external_urls["spotify"]
+      t.duration_ms = spot_track.duration_ms
+      t.popularity = popularity
+      t.audio_features = audio_features.to_json
+      t.album = album
+      t.artists = artists
+      t.duration_ms = spot_track.duration_ms
     end
 
-    pt = PlaylistTrack.find_by(playlist: playlist, track: track)
-    unless pt.present?
-      pt = PlaylistTrack.create!(playlist: playlist, track: track, added_at: spot_playlist.tracks_added_at[spot_track.id].in_time_zone)
+    PlaylistTrack.find_or_create_by!(playlist: playlist, track: track) do |pt|
+      pt.added_at = spot_playlist.tracks_added_at[spot_track.id].in_time_zone
     end
   end
 
   def build_album spot_album
     Rails.logger.info "  build_album: #{spot_album.name}"
-    album = Album.find_by(spotify_id: spot_album.id)
-
-    unless album.present?
-      artists = build_artists spot_album.artists
-      popularity = try_fetch(spot_album, :popularity)#spot_album.popularity
-      release_date = try_fetch(spot_album, :release_date)#spot_album.release_date
-      album = Album.create!(spotify_id: spot_album.id, name: spot_album.name, release_date: release_date, popularity: popularity, url: spot_album.external_urls["spotify"], artists: artists)
+    Album.find_or_create_by!(spotify_id: spot_album.id) do |a|
+      popularity = try_fetch(spot_album, :popularity) # spot_album.popularity
+      release_date = try_fetch(spot_album, :release_date) # spot_album.release_date
+      a.name = spot_album.name,
+        a.release_date = release_date,
+        a.popularity = popularity,
+        a.url = spot_album.external_urls["spotify"]
     end
-    album
   end
 
   def build_artists spot_artists
@@ -99,7 +96,6 @@ class BuildMusicNetService
     playlists.flatten!
   end
 
-  private
   def try_fetch(object, attribute)
     result = nil
     begin
@@ -109,6 +105,5 @@ class BuildMusicNetService
     end
     result
   end
-
 
 end
