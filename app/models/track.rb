5f2ea3048c3a4f9fa6d6e76b45d40eb589bc5a0f
chrigu <christian.muehlethaler@optor.ch>
2023-09-02 01:09:18 +0200

    Energie und Tempo in die Listview

--- app/models/track.rb
+++ app/models/track.rb
@@ -12,14 +12,14 @@ class Track < ApplicationRecord
 
   # Siehe @RSpotify::Audiofeatures
   # - acousticness
-  # - danceability
+  # [Float] danceability Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable.
   # - mode  1: Dur, 0 Minor
   # - energy
   # - instrumentalness
   # - liveness
   # - loudness
   # - speechiness
-  # - tempo
+  # - [Float] tempo The overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration.
   # - time_signature
   # - valence
   #
@@ -32,6 +32,13 @@ class Track < ApplicationRecord
   # - uri
   def af
     @af ||= audio_features.present? ? JSON.parse(audio_features, object_class: OpenStruct) : nil
+  end
+
+  def energy
+    af.try(:energy)
+  end
+  def tempo
+    af.try(:tempo)
   end
 
   #{"acousticness"=>0.552, "analysis_url"=>"https://api.spotify.com/v1/audio-analysis/2uSavRrWjouarU9DupcWmK", "danceability"=>0.69, "duration_ms"=>296333, "energy"=>0.553, "instrumentalness"=>0.914, "key"=>5, "liveness"=>0.121, "loudness"=>-12.152, "mode"=>1, "speechiness"=>0.0372, "tempo"=>131.674, "time_signature"=>4, "track_href"=>"https://api.spotify.com/v1/tracks/2uSavRrWjouarU9DupcWmK", "valence"=>0.917, "external_urls"=>nil, "href"=>nil, "id"=>"2uSavRrWjouarU9DupcWmK", "type"=>"audio_features", "uri"=>"spotify:track:2uSavRrWjouarU9DupcWmK"}
