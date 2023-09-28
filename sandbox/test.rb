7c131db448da4882b0b2b47b8f11b859d25387d6
Christian Mühlethaler <christian.muehlethaler@optor.ch>
2023-09-28 16:06:08 +0200

    test.rb etwas umgeschrieben

--- sandbox/test.rb
+++ sandbox/test.rb
@@ -1,19 +1,21 @@
-
 require 'rspotify'
 
-# Siehe Rails.application.credentials.dig(:spotify, :client_id)
-# Die Credentials sind https://developer.spotify.com/dashboard/61f2f8a2eb7340e89e33723785125ca5 hinterlegt
-credentials = {
-  client_id: "ddd",
-  client_secret: "xxx"
-}
+def test_rspotify
+  # Siehe Rails.application.credentials.dig(:spotify, :client_id)
+  # Die Credentials sind https://developer.spotify.com/dashboard/61f2f8a2eb7340e89e33723785125ca5 hinterlegt
+  credentials = {
+    client_id: "ddd",
+    client_secret: "xxx"
+  }
 
+  RSpotify.authenticate(credentials.dig(:client_id), credentials.dig(:client_secret))
 
-RSpotify.authenticate(credentials.dig(:client_id), credentials.dig(:client_secret))
+  me = RSpotify::User.find('hrigu')
 
-me = RSpotify::User.find('hrigu')
-
-# Alle öffentliche und auf das User Profil hinzugefügte Playlists.
-me.playlists.each do |playlist|
-  puts playlist.name
+  # Alle öffentliche und auf das User Profil hinzugefügte Playlists.
+  me.playlists.each do |playlist|
+    puts playlist.name
+  end
 end
+
+#test_rspotify
