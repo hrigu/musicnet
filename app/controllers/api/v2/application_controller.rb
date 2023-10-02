c65d52b99dfa99a0b5f0ea1633bd1acca4ec6931
chrigu <christian.muehlethaler@optor.ch>
2023-10-02 21:01:54 +0200

    der application_controller.rb für die graphiti Controller erbt nun von ActionController::API

--- app/controllers/api/v2/application_controller.rb
+++ app/controllers/api/v2/application_controller.rb
@@ -1,3 +1,3 @@
-class Api::V2::ApplicationController < ActionController::Base # TODO in Planik ist die SUperklasse ActionController::API
+class Api::V2::ApplicationController < ActionController::API # TODO in Planik ist die SUperklasse ActionController::API
   include Graphiti::Rails::Responders
 end
