fa532a288af5dd5b0bb79df311223cabda5fcf6d
chrigu <christian.muehlethaler@optor.ch>
2023-10-14 17:23:41 +0200

    JS bootstrap eingebunden

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,11 @@
+
+
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+import "popper"
+import "bootstrap"
 import "@hotwired/turbo-rails"
 import "controllers"
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
