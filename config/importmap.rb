7cf6bf10851531e8d3332a92ba4199fbc5f6f598
chrigu <christian.muehlethaler@optor.ch>
2023-10-18 18:21:20 +0200

    handsontable hinzugefügt

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "popper", to: 'popper.js', preload: true
 pin "bootstrap", to: 'bootstrap.min.js', preload: true
+pin "handsontable", to: "https://cdn.jsdelivr.net/npm/handsontable/dist/handsontable.full.min.js"
 
 pin_all_from "app/javascript/controllers", under: "controllers"
