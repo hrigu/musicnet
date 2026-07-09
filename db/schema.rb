5031327bc2bbd1714d2e0a4c2e9a43915d50d5fc
chrigu <chmuehlethaler@gmail.com>
2026-07-09 16:54:33 +0200

    Tags: Intent 86 abschliessen
    Markiert den Abschluss von Intent 86, verschiebt das Intent nach completed und uebernimmt die mechanischen Schema-Korrekturen aus dem finalen Rubocop-Lauf. Relevante Specs und bundle exec rubocop sind gruen.

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_09_103002) do
+ActiveRecord::Schema[8.1].define(version: 20_260_709_103_002) do
   create_table "albums", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "name"
@@ -59,7 +61,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_09_103002) do
     t.integer "library_id", null: false
     t.integer "playlist_id", null: false
     t.datetime "updated_at", null: false
-    t.index ["library_id", "playlist_id"], name: "index_library_playlists_on_library_id_and_playlist_id", unique: true
+    t.index %w[library_id playlist_id], name: "index_library_playlists_on_library_id_and_playlist_id", unique: true
     t.index ["library_id"], name: "index_library_playlists_on_library_id"
     t.index ["playlist_id"], name: "index_library_playlists_on_playlist_id"
   end
@@ -99,8 +101,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_09_103002) do
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
     t.index ["tag_id"], name: "index_tag_assignments_on_tag_id"
-    t.index ["user_id", "created_at"], name: "index_tag_assignments_on_user_id_and_created_at"
-    t.index ["user_id", "tag_id", "created_at"], name: "index_tag_assignments_on_user_id_and_tag_id_and_created_at"
+    t.index %w[user_id created_at], name: "index_tag_assignments_on_user_id_and_created_at"
+    t.index %w[user_id tag_id created_at], name: "index_tag_assignments_on_user_id_and_tag_id_and_created_at"
     t.index ["user_id"], name: "index_tag_assignments_on_user_id"
   end
 
@@ -111,7 +113,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_09_103002) do
     t.datetime "created_at", null: false
     t.string "name", null: false
     t.datetime "updated_at", null: false
-    t.index ["category_id", "name"], name: "index_tags_on_category_id_and_name", unique: true
+    t.index %w[category_id name], name: "index_tags_on_category_id_and_name", unique: true
     t.index ["category_id"], name: "index_tags_on_category_id"
   end
 
@@ -122,7 +124,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_09_103002) do
     t.integer "track_id", null: false
     t.datetime "updated_at", null: false
     t.index ["tag_id"], name: "index_track_tags_on_tag_id"
-    t.index ["track_id", "tag_id"], name: "index_track_tags_on_track_id_and_tag_id", unique: true
+    t.index %w[track_id tag_id], name: "index_track_tags_on_track_id_and_tag_id", unique: true
     t.index ["track_id"], name: "index_track_tags_on_track_id"
   end
 
