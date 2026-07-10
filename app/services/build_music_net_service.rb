7ebb5cfb1436f3d0f91292f8de1a7a7cb607f908
chrigu <chmuehlethaler@gmail.com>
2026-07-10 13:45:54 +0200

    ServiceInfo aus BuildMusicNetService in eigene Datei auslagern
    Reiner Umzug ohne Verhaltensänderung, folgt Zeitwerks Namespacing-Konvention
    (app/services/build_music_net_service/service_info.rb -> BuildMusicNetService::ServiceInfo).

--- app/services/build_music_net_service.rb
+++ app/services/build_music_net_service.rb
@@ -257,45 +257,4 @@ class BuildMusicNetService
 
   # Gebündelt vorgeladene Spotify-Details (je ein Hash spotify_id → Objekt), siehe prefetch_details
   Prefetched = Struct.new(:albums, :artists)
-
-  class ServiceInfo
-    attr_reader :hash
-
-    def initialize
-      @hash = {}
-    end
-
-    def add_new_created_playlist(name)
-      add(playlists: { created: name })
-    end
-
-    def add_new_created_track(name)
-      add(tracks: { created: name })
-    end
-
-    def add_new_created_album(name)
-      add(albums: { created: name })
-    end
-
-    def add_new_created_artist(name)
-      add(artists: { created: name })
-    end
-
-    def add_renamed_playlist(old_name, new_name)
-      add(playlists: { renamed: [old_name, new_name] })
-    end
-
-    # what is a hash mit einem Eintrag {playlists: {created: "yz"})}
-    def add(what)
-      key = what.keys.first
-      @hash[key] = {} unless @hash.key? key
-      hash_value = @hash[key]
-      value = what[key] # ist ein hash
-
-      value.each do |k, v|
-        hash_value[k] = [] unless hash_value.key? k
-        hash_value[k] << v
-      end
-    end
-  end
 end
